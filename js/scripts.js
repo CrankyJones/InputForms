6ff7d97ef6c58d96b0b60a9e1ab3e8db5baafc38
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-01-13 15:51:08 -0800

    Debug js and html
    Co-authored-by: Jon Stump <jmstump@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-  $("formName").submit(function(event) {
-    event.prevenDefault();
+  $("#formName").submit(function(event) {
+    event.preventDefault();
     const person = $("input#person").val();
-    const favoriteFood = $("input#favoriteFood").val();
+    const favoriteFood = $("#favoriteFood").val();
     const music = $("input:radio[name=music]:checked").val();
     const born = $("input#born").val();
 
-    $("#person").text(person);
-    $("#favoriteFood").text(favoriteFood);
-    $("#music").text(music);
-    $("#born").text(born);
+    $(".person").text(person);
+    $(".favoriteFood").text(favoriteFood);
+    $(".music").text(music);
+    $(".born").text(born);
 
     $("#bio").show();
   });
