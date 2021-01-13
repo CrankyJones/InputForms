8376357277917901c7f4ea93df23ad5379fbf533
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-01-13 15:33:46 -0800

    Add js and css
    Co-authored-by: Jon Stump <jmstump@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-$(document)
+$(document).ready(function() {
+  $("formName").submit(function(event) {
+    event.prevenDefault();
+    const person = $("input#person").val();
+    const favoriteFood = $("input#favoriteFood").val();
+    const music = $("input:radio[name=music]:checked").val();
+    const born = $("input#born").val();
+
+    $("#person").text(person);
+    $("#favoriteFood").text(favoriteFood);
+    $("#music").text(music);
+    $("#born").text(born);
+
+    $("#bio").show();
+  });
+});
